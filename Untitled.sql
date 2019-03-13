c010a91f5276b273f52660f2e90537de426333a7
Andrayayay <andrayayay@gmail.com>
2019-03-13 17:04:26 -0500

    update

--- Untitled.sql
+++ Untitled.sql
@@ -11,3 +11,33 @@ CREATE TABLE products (
     PRIMARY KEY (id)
 );
 
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("iPhone Charger", "Electronics", 7.99, 5000);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Hydro Flask", "Essentials", 30.99, 1200);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Scrabble", "Games", 12.99, 800);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Women's Daily Multi Vitamin", "Health", 34.99, 500);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Matcha Green Tea Powder", "Food", 20.99, 1000);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Flower Press", "Crafts", 40.99, 250);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Catnip", "Pets", 6.99, 600);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Orgain Protein Powder", "Food", 29.99, 2500);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Apple TV", "Electronics", 149.99, 5000);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Prismacolor Pencils", "Crafts", 49.99, 1500);
+
